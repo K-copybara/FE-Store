76c946c260237c0a15b1d95d294752c93d99b9dc
seoyonara <syp614@ewhain.net>
2025-10-08 18:42:20 +0900

    🔥 AuthStore 삭제

--- src/pages/Auth/PrivateRoute.jsx
+++ src/pages/Auth/PrivateRoute.jsx
@@ -1,14 +1,44 @@
 import { Navigate, Outlet, useLocation } from 'react-router-dom';
 import { useEffect, useState } from 'react';
-import { useAuthStore } from '../../store/authStore';
+import styled from 'styled-components';
 
 export default function PrivateRoute() {
-  const { isLoggedIn } = useAuthStore();
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const [isLoading, setIsLoading] = useState(true);
+
   const location = useLocation();
+
+  useEffect(() => {
+    const checkAuth = () => {
+      const token = JSON.parse(localStorage.getItem('token'));
+
+      if (!token) {
+        setIsLoggedIn(false);
+      } else {
+        setIsLoggedIn(true);
+      }
+
+      setIsLoading(false);
+    };
+
+    checkAuth();
+  }, []);
+
+  if (isLoading) return null;
 
   if (!isLoggedIn) {
     return <Navigate to="/login" replace state={{ from: location }} />;
   }
-
-  return <Outlet />;
+  return (
+    <Wrapper>
+      <Outlet />
+    </Wrapper>
+  );
 }
+
+const Wrapper = styled.div`
+  width: 100%;
+  height: 100%;
+  padding: 1.25rem 1.5rem;
+`;
