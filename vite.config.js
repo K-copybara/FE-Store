bec5362c5d4c7fb8970b499d5588dcd97ae5ffec
seoyonara <syp614@ewhain.net>
2025-10-17 20:43:35 +0900

    ♻️ 포트 번호 변경

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,10 @@ import svgr from 'vite-plugin-svgr';
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    port: 3000,
+    open: true,
+    host: true,
+  },
   plugins: [react(), svgr()],
 });
